057b71d1adda51f3d1558af51ad62d59b132290d
Suvorov <jack.suvorov13@gmail.com>
2022-07-29 16:13:11 +0300

    jenkins

--- src/main/java/org/example/pages/BasePage.java
+++ src/main/java/org/example/pages/BasePage.java
@@ -1,14 +1,30 @@
 package org.example.pages;
 
+import org.example.utils.PropertiesLoader;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage  {
+import java.time.Duration;
 
-    public static final String BASE_URL = "https://www.saucedemo.com/";
+public class BasePage {
 
+    String baseUrl;
     WebDriver driver;
+    WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.baseUrl = PropertiesLoader.loadProperties().getProperty("login.url");
+    }
+
+    public void waitForPageLoaded() {
+        new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
+            }
+        };
     }
 }
